77e5cb1f53e2178f049ee0ab7851f8081a3e2884
Gabor Szabo <gabor@szabgab.com>
2023-07-29 21:52:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,10 +1,10 @@
 //mod demo_str;
-//mod demo_string;
+mod demo_string;
 fn main() {
     //println!("Hello World");
     //demo_str::run();
-    //demo_string::run();
-    run();
+    demo_string::run();
+    //run();
 }
 
 #[derive(Debug)]
