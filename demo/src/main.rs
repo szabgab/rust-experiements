3cd0aa3707f5996922a29beefe4581990e2c2c5c
Gabor Szabo <gabor@szabgab.com>
2023-07-29 09:20:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,18 +1,13 @@
-fn main() {
-    demo_str::run();
-
-    other1::run_this();
-    other2::say_hello();
-    say_hello();
-}
-
-
-mod other1 {
-    pub fn run_this() {
-        dbg!("other1 run_this");
-    }
-}
-
-mod other2;
-use other2::say_hello;
 mod demo_str;
+fn main() {
+    println!("Hello World");
+    //demo_str::run();
+
+    // other2::say_hello();
+    // say_hello();
+}
+
+
+
+// mod other2;
+// use other2::say_hello;
