4c47ceb4bc487b8a4d31e5391f48d7e9a324ba54
Gabor Szabo <gabor@szabgab.com>
2023-07-29 12:02:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -17,17 +17,28 @@ struct Color {
 }
 
 fn run() {
-    let mut red = Color {red: 255, green: 0, blue: 0, name: "red".to_string()};
-    dbg!(&red);
-    red.red = 250;
-    dbg!(&red);
+    // let mut red = Color {red: 255, green: 0, blue: 0, name: "red".to_string()};
+    // dbg!(&red);
+    // red.red = 250;
+    // dbg!(&red);
 
-    let mut blue = get_blue();
-    dbg!(&blue);
-    blue.blue = 250;
-    dbg!(&blue);
+    // let mut blue = get_blue();
+    // dbg!(&blue);
+    // blue.blue = 250;
+    // dbg!(&blue);
+
+    let colors = get_colors();
+    dbg!(&colors);
 }
 
 fn get_blue() -> Color {
     Color {red: 0, green: 0, blue: 255, name: "blue".to_string()}
+}
+
+fn get_colors() -> Vec<Color> {
+    let colors = vec![
+        Color {red: 255, green: 0, blue: 0, name: "red".to_string()},
+        Color {red: 0, green: 0, blue: 255, name: "blue".to_string()},
+    ];
+    colors
 }
