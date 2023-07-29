5d8886d19bccdb850ee20d348c8c33d33fb24be4
Gabor Szabo <gabor@szabgab.com>
2023-07-29 11:52:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,8 +1,21 @@
 //mod demo_str;
-mod demo_string;
+//mod demo_string;
 fn main() {
     //println!("Hello World");
     //demo_str::run();
-    demo_string::run();
+    //demo_string::run();
+    run();
+}
+
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+    name: String,
+}
+
+fn run() {
+    let red = Color {red: 255, green: 0, blue: 0, name: "red".to_string()};
+    dbg!(red);
 }
 
