39c1c20b5bc6dea7634d9cd3981bd05d4b4d3902
Gabor Szabo <gabor@szabgab.com>
2023-07-29 09:10:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,14 +4,22 @@ fn main() {
     let var1 = "Bar";
     run(var1);
     dbg!(var1);
+    other1::run_this();
+    other2::run_this();
 }
 
 fn run(name :&str) {
     dbg!(name);
 }
 
-mod other {
-    fn run_this() {
-        dbg!("run_this");
+mod other1 {
+    pub fn run_this() {
+        dbg!("other1 run_this");
+    }
+}
+
+mod other2 {
+    pub fn run_this() {
+        dbg!("other2 run_this");
     }
 }
---
+++ demo/src/other2.rs
@@ -0,0 +1,3 @@
+pub fn run_this() {
+    dbg!("other2 run_this");
+}
