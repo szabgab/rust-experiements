8a3cfa73f25d5f7ce9d65de28ea3d785bc5e1f5e
Gabor Szabo <gabor@szabgab.com>
2023-07-29 10:24:01 +0300

    autocommit

--- demo/src/demo_string.rs
+++ demo/src/demo_string.rs
@@ -14,3 +14,13 @@ pub fn run() {
 //     println!("Hello {name}");
 //     println!("Hello {name}");
 // }
+
+
+
+// let mut name = "Foo".to_string();
+// println!("{name}");
+// let lname = "Bar".to_string();
+// name.push_str(&lname);
+// let other = &name;  // it works here, but we cannot do this before calling push_str!
+// println!("{other}");
+// println!("{name}");
