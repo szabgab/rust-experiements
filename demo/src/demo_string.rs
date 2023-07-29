42036650ad1c1f0c509d14dff70e828958fdcdd2
Gabor Szabo <gabor@szabgab.com>
2023-07-29 10:42:01 +0300

    autocommit

--- demo/src/demo_string.rs
+++ demo/src/demo_string.rs
@@ -1,24 +1,32 @@
 pub fn run() {
-    let fname = "Foo".to_string();
-    let lname = "Bar".to_string();
-    println!("{fname}");
-    println!("{lname}");
-    say_hello(&fname);
-    // println!("This is {name}");
-    let res = combine(&fname, &lname);
-    println!("Combined name {res}");
-    println!("{fname}");
-    println!("{lname}");
-}
-fn say_hello(name: &str) {
-    println!("Hello {name}");
-    println!("Hello {name}");
+    let name = "Foo".to_string();
+    println!("{name}");
+    //take_ownership(name);
+
 }
 
-fn combine(fname: &str, lname: &str) -> String {
-    //format!("{fname} {lname}")
-    fname.to_string() + " " + lname
-}
+
+// pub fn run() {
+//     let fname = "Foo".to_string();
+//     let lname = "Bar".to_string();
+//     println!("{fname}");
+//     println!("{lname}");
+//     say_hello(&fname);
+//     // println!("This is {name}");
+//     let res = combine(&fname, &lname);
+//     println!("Combined name {res}");
+//     println!("{fname}");
+//     println!("{lname}");
+// }
+// fn say_hello(name: &str) {
+//     println!("Hello {name}");
+//     println!("Hello {name}");
+// }
+
+// fn combine(fname: &str, lname: &str) -> String {
+//     //format!("{fname} {lname}")
+//     fname.to_string() + " " + lname
+// }
 
 
 // let mut name = "Foo".to_string();
