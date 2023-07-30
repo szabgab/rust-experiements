2c207a551860758f6d59e165a6427268a7101b3e
Gabor Szabo <gabor@szabgab.com>
2023-07-30 07:58:01 +0300

    autocommit

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -11,26 +11,6 @@ fn main() {
     //demo_str::run();
     //demo_string::run();
     //run();
-
-    // let mut graph = get_graph();
-    // dbg!(&graph);
-    // move_graph(&mut graph);
-    // dbg!(&graph);
-}
-
-fn get_graph() -> Vec<Point> {
-    vec![
-        Point {x: 0, y: 0},
-        Point {x: 10, y: 20},
-        Point {x: 30, y: 40},
-    ]
-
-}
-
-fn move_graph(graph: &mut Vec<Point>) {
-    for point in graph {
-        point.x += 100;
-    }
 }
 
 
