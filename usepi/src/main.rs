e97905e97eb72598985eb0bf8b5690110f031491
Gabor Szabo <gabor@szabgab.com>
2023-08-25 09:40:01 +0300

    autocommit

--- usepi/src/main.rs
+++ usepi/src/main.rs
@@ -1,9 +1,12 @@
 use std::f32::consts::PI;
 use std::f64::consts::PI as bigPI;
+use std::f64::consts;
 
 fn main() {
     println!("{}", std::f32::consts::PI);
     println!("{}", std::f64::consts::PI);
     println!("{}", PI);
     println!("{}", bigPI);
+    println!("{}", consts::PI);
+    println!("{}", consts::E);
 }
