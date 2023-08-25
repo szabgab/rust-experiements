4d5298d82859534d448af4b9cda66c3087c52c50
Gabor Szabo <gabor@szabgab.com>
2023-08-25 09:28:01 +0300

    autocommit

--- libriaries/src/main.rs
+++ libriaries/src/main.rs
@@ -1,3 +1,6 @@
+use std::f32::consts::PI;
 fn main() {
-    println!("{}", std::f32::);
+    println!("{}", std::f32::consts::PI);
+    println!("{}", std::f64::consts::PI);
+    println!("{}", PI);
 }
